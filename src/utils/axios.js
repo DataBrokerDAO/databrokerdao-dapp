3f7c0ddc326c9f9dd4b2574416ec510281d3c50f
PJ <peter-jan@settlemint.com>
2018-09-19 14:09:45 +0200

    fix: update anonymousJWT token

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -25,7 +25,7 @@ export default function getAxios(
     });
   } else if (anonymous) {
     const anonymousJWTToken =
-      'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ3YWxsZXRJZCI6IjViN2FiMmU0OTdhNzllMDAyNjMwMmNjNSIsImlhdCI6MTUzNjA1NTEyN30.yUFYgc4vya9R12FIYvu2KCmGPRwEFJkZDhGh1UY5msk';
+      'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiI1YmEyM2JmNDhlODUyODAwMjU3ZGRkMTciLCJpYXQiOjE1MzczNTg5MTR9.ZAzNsbMrqkPqn8tyKROExWjcHZaj1P85zriNrjvGjGU';
     instance = axios.create({
       baseURL: process.env.REACT_APP_DAPI_URL,
       headers: { Authorization: anonymousJWTToken }
