c77ccb6756d9888c03670e4c28cd620a6e1cd1bc
PJ <peter-jan@settlemint.com>
2018-10-11 21:44:22 +0200

    fix: ceil

--- src/redux/purchases/actions.js
+++ src/redux/purchases/actions.js
@@ -93,7 +93,7 @@ export const PURCHASES_ACTIONS = {
         if (endTime === 0) {
           purchasePrice = stream.price;
         } else {
-          const durationS = moment(endTime).diff(moment()) / 1000;
+          const durationS = Math.ceil(moment(endTime).diff(moment()) / 1000);
           purchasePrice = stream.price * durationS;
         }
 
